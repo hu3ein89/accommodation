4697802a77c5a5eda1747a12dde6c942b8c3de9a
hu3ein89 <halavi8968@gmail.com>
2025-08-09 13:08:04 +0430

    fix: update api/index.js to be more resilient on Vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import jsonServer from 'json-server';
-import path from 'path';
+
+// Import the database file directly
+import db from '../db.json';
 
 const server = express();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router(db); // Use the imported database object
 const middlewares = jsonServer.defaults();
 
 // Use the default json-server middlewares
@@ -11,8 +13,5 @@ server.use(middlewares);
 
 // Use the router for all requests to the /api endpoint
 server.use('/api', router);
-
-// Serve your frontend build files
-server.use(express.static(path.join(__dirname, 'dist')));
 
 export default server;
