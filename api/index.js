2ec176a32318fd10f4663542767fac3e93f14d72
hu3ein89 <halavi8968@gmail.com>
2025-08-09 21:55:00 +0430

    chore: add debug log to check db keys

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ const dbPath = path.join(process.cwd(), 'db.json');
 
 // Read and parse the db.json file
 const data = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+console.log('Available routes:', Object.keys(data));
 
 // Pass the JavaScript object to the router, creating an in-memory database
 const router = jsonServer.router(data);
